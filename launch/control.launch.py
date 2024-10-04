cf4b7de32a004d5f41aca6f7915b6007c746d8cb
Luna Blind <luna.blind@protonmail.com>
2024-10-04 16:45:45 +0200

    Added params for apriltag + rviz feature
    + put launch of object detection and control in one launch file

--- launch/control.launch.py
+++ launch/control.launch.py
@@ -10,11 +10,23 @@ from launch.actions import (
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+from hippo_common.launch_helper import (
+    LaunchArgsDict,
+    declare_vehicle_name_and_sim_time,
+)
+
 
 def generate_launch_description() -> LaunchDescription:
     launch_description = LaunchDescription()
     arg = DeclareLaunchArgument('vehicle_name')
     launch_description.add_action(arg)
+
+    action = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='true',
+        description='The name of the camera.',
+    )
+    launch_description.add_action(action)
 
     package_path = get_package_share_path('position_control')
     param_file_path = str(package_path / 'config/controller_params.yaml')
@@ -38,11 +50,21 @@ def generate_launch_description() -> LaunchDescription:
                  ]),
     ])
 
-    gripper_launchfile_path = str(package_path / 'launch/gripper.launch.py')
-    gripper_launch = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([gripper_launchfile_path])
+    # gripper_launchfile_path = str(package_path / 'launch/gripper.launch.py')
+    # gripper_launch = IncludeLaunchDescription(
+    #         PythonLaunchDescriptionSource([gripper_launchfile_path])
+    #         )
+    # launch_description.add_action(gripper_launch)
+
+    
+    args = LaunchArgsDict()
+    args.add_vehicle_name_and_sim_time()
+
+    detection_launchfile_path = str(package_path / 'launch/object_detection.launch.py')
+    detection_launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([detection_launchfile_path]), launch_arguments=args.items()
             )
-    launch_description.add_action(gripper_launch)
+    launch_description.add_action(detection_launch)
     
     launch_description.add_action(group)
     return launch_description
--- launch/object_detection.launch.py
+++ launch/object_detection.launch.py
@@ -1,5 +1,11 @@
 from launch_ros.actions import Node, PushRosNamespace
 
+from hippo_common.launch_helper import (
+    LaunchArgsDict,
+    declare_vehicle_name_and_sim_time,
+)
+
+
 from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
 from launch.actions import (
@@ -9,6 +15,22 @@ from launch.actions import (
 from launch.substitutions import LaunchConfiguration
 
 
+def create_apriltag_viz_node():
+    args = LaunchArgsDict()
+    args.add_vehicle_name_and_sim_time()
+    args['line_thickness'] = 5
+    args['alpha'] = 0.5
+    return Node(
+        package='apriltag_viz',
+        executable='apriltag_viz',
+        namespace=LaunchConfiguration('camera_name'),
+        # namespace='front_camera',
+        parameters=[args],
+        emulate_tty=True,
+        output='screen',
+    )
+
+
 def generate_launch_description() -> LaunchDescription:
     launch_description = LaunchDescription()
     arg = DeclareLaunchArgument('vehicle_name')
@@ -16,6 +38,28 @@ def generate_launch_description() -> LaunchDescription:
 
     # package_path = get_package_share_path('position_control')
     # kf_params_file_path = str(package_path / 'config/kalman_filter_params.yaml')
+    pkg_path = get_package_share_path('position_control')
+    default_path = str(pkg_path / 'config/apriltag_config.yaml')
+    action = DeclareLaunchArgument(
+        name='apriltag_config_file',
+        default_value=default_path,
+        description='Path to the apriltag_config.yaml file.',
+    )
+    launch_description.add_action(action)
+
+    action = DeclareLaunchArgument(
+        name='camera_name',
+        default_value='front_camera',
+        description='The name of the camera.',
+    )
+    launch_description.add_action(action)
+
+    action = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='true',
+        description='The name of the camera.',
+    )
+    launch_description.add_action(action)
 
     group = GroupAction([
         PushRosNamespace(LaunchConfiguration('vehicle_name')),
@@ -26,12 +70,23 @@ def generate_launch_description() -> LaunchDescription:
              remappings=[
                  ("image_rect", "synchronized_image"),
                  ("camera_info", "synchronized_camera_info"),
+                 # ("image_rect", "front_camera/image_rect"),
+                 # ("camera_info", "front_camera/camera_info"),
                  ],
              parameters=[
-                 {'camera_frame': 'front_camera_link'},
+                LaunchConfiguration('apriltag_config_file'),
+                {'pose_method': 'solve_pnp'},
+                 {'camera_frame': 'front_camera'},
                  {'tag_family': 'tag36h11'},
                  {'publish_tf': True}
-             ]),
+            ],
+
+             # parameters=[
+             #     {'camera_frame': 'front_camera_link'},
+             #     {'tag_family': 'tag36h11'},
+             #     {'publish_tf': True}
+             # ]
+             ),
         Node(executable='object_detection.py',
              package='position_control',
              output='screen',
@@ -39,7 +94,8 @@ def generate_launch_description() -> LaunchDescription:
         Node(executable='image_camera_synchronizer.py',
              package='position_control',
              output='screen',
-             )
+             ),
+        create_apriltag_viz_node(),
         # Node(executable='ranges_debugger.py', package='position_control')
         # add more here
     ])
